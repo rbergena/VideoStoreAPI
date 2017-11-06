0e09124690a74c4835590e7300f625b1a5471d03
Rebecca Bergena <rbergena@gmail.com>
2017-11-06 15:30:39 -0800

    add customer tests and movies_checked_out column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106213616) do
+ActiveRecord::Schema.define(version: 20171106221254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20171106213616) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "account_credit"
+    t.integer "movies_checked_out_count", default: 0
   end
 
   create_table "movies", force: :cascade do |t|
