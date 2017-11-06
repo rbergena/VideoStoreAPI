c56ca4b80f81fdeb31b2e3bf03b74444594c968b
Bennett Rahn <rahnibennett@gmail.com>
2017-11-06 13:46:31 -0800

    set up basic controller/routes

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "returns and array of all Movies" do
+      get movies_path
+      must_respond_with :success
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+      body.length.must_equal Movie.count
+    end
+
+    it  "returns an empty array when no Movies" do
+      Movie.destroy_all
+      get movies_path
+      must_respond_with :success
+
+      body = JSON.parse(response.body)
+      body.must_be empty?
+    end
+  end
 end
