d6f348248735dcb23f11d34aeace276c023f2350
Rebecca Bergena <rbergena@gmail.com>
2017-11-07 10:39:09 -0800

    rentals checkout action and rentals fixtures

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -4,10 +4,30 @@ class RentalsController < ApplicationController
   end
 
   def checkout
+    rental = Rental.new(rental_params)
 
+    if rental.save
+      render(
+        json: {
+          customer_id: rental.customer_id,
+          movie_id: rental.movie_id,
+          due_date: rental.due_date
+        },
+        status: :ok
+      )
+    else
+      render(
+        json: { errors: rental.errors.messages },
+        status: :bad_request
+      )
+    end
   end
 
   def checkin
 
+  end
+
+  def rental_params
+    params.permit(:customer_id, :movie_id, :due_date)
   end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+
+  describe "create" do
+    let(:rental_data) {
+      {
+        movie_id: Movie.first.id,
+        customer_id: Customer.first.id,
+        due_date: DateTime.now
+      }
+    }
+
+    it "creates a rental" do
+      Rental.new(rental_data).must_be :valid?
+
+      proc {
+        post checkout_path, params: rental_data
+      }.must_change 'Rental.count', 1
+
+      must_respond_with :success
+    end
+
+    it "responds with bad_request if invalid data" do
+      invalid_rental_data = {
+        customer_id: Customer.first.id,
+        due_date: DateTime.now
+      }
+
+      proc {
+        post checkout_path, params: invalid_rental_data
+      }.wont_change 'Rental.count'
+      must_respond_with :bad_request
+
+      body = JSON.parse(response.body)
+      body.must_equal "errors" => {"movie" => ["must exist"]}
+    end
+  end
 end
